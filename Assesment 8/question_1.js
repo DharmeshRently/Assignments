417091954053f3342341fadbd52eafd2a6211f57
dharmesh <dharmesh52741@gmail.com>
2023-05-17 09:21:47 +0530

    Assignment 8

---
+++ Assesment 8/question_1.js
@@ -0,0 +1,14 @@
+// 1. Write a program that takes a date and prints the day on which the date falls on. 1
+
+var date=new Date()
+var day
+switch(date.getDay()){
+    case 0: day='Sunday'; break;
+    case 1: day='Monday'; break;
+    case 2: day='Tuesday'; break;
+    case 3: day='Wednesday'; break;
+    case 4: day='Thursday'; break;
+    case 5: day='Friday'; break;
+    case 6: day='Saturday'; break;
+}
+console.log(day)
---
+++ Assesment 8/question_4.js
@@ -0,0 +1,4 @@
+// 4. Demonstrate replace all occurrences of a string within a sentence.
+
+var str="hello world  or welcome"
+console.log(str.replace(/l/g,"L")) // here g flag denotes for a global search
